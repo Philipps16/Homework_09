5f7d79855465c2d5a4af25e8d3923ceb7d75fcf5
Philipp Mitrofanov <press.fm@mail.ru>
2023-01-11 21:18:34 +0300

    Выполнил упражнение 01

---
+++ Example_01/Program.cs
@@ -0,0 +1,21 @@
+﻿string PrintNumbers(int start, int end)
+{
+if (start == end)
+{
+return start.ToString();
+}
+return (end.ToString() + ' ' + PrintNumbers(start, end - 1));
+}
+
+
+
+
+
+Console.Clear();
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine(PrintNumbers(n, m));
+
