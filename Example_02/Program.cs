c8932af6a67511625f1889cc156e7105e32de7ff
Philipp Mitrofanov <press.fm@mail.ru>
2023-01-11 21:34:37 +0300

    Выполнил упражнение 02

---
+++ Example_02/Program.cs
@@ -0,0 +1,23 @@
+﻿int SumNumbers(int start, int end)
+{
+if (end == start)
+{
+return end;
+}
+end = end + SumNumbers (start, end - 1); 
+return end;
+
+}
+
+
+
+
+
+Console.Clear();
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumNumbers(n, m));
+
