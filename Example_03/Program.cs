bb664c698b66bce1f2f1d4a352b55663e39b17aa
Philipp Mitrofanov <press.fm@mail.ru>
2023-01-11 22:05:56 +0300

    Выполнил упражнение 03

---
+++ Example_03/Program.cs
@@ -0,0 +1,37 @@
+﻿int Function (int number, int argument)
+{
+    if (number == 0)
+    return argument + 1;
+    else
+        if (number != 0 && argument == 0)
+        return (Function (number - 1, 1));
+        else
+            return (Function (number - 1, Function (number, argument - 1)));
+
+    return number;    
+}
+
+
+
+
+
+
+
+
+Console.Clear();
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+if ( n <= 0)
+{
+    Console.Write ("Введите НЕОТРИЦАТЕЛЬНОЕ число: ");
+    n = int.Parse(Console.ReadLine());
+}
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+if ( m <= 0)
+{
+    Console.Write ("Введите НЕОТРИЦАТЕЛЬНОЕ число: ");
+    m = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"{n}, {m} -> A ({n},{m}) = {Function(n, m)}");
